7121f9875c99d6a2b3b8405fb6fe3c5863ff9015
mrst2000 <75087475+mrst2000@users.noreply.github.com>
2024-04-29 20:36:25 +0000

    Update deploy_macos.sh

--- libs/deploy_macos.sh
+++ libs/deploy_macos.sh
@@ -16,14 +16,14 @@ mkdir -p $DEST
 pushd download-artifact
 find . -name artifacts.tgz | xargs -n1 tar xvzf
 mv deployment/public_res/* deployment/macos-$ARCH
-mv deployment/macos-$ARCH/* $BUILD/nekoray.app/Contents/MacOS
+mv deployment/macos-$ARCH/* $BUILD/MrLinux.app/Contents/MacOS
 popd
 
 #### deploy qt & DLL runtime => .app ####
 pushd $BUILD
-macdeployqt nekoray.app -verbose=3
+macdeployqt MrLinux.app -verbose=3
 popd
 
 #### pack dmg ###
 sudo npm install -g appdmg
-appdmg appdmg.json $DEST/nekoray.dmg
+appdmg appdmg.json $DEST/MrLinux.dmg
